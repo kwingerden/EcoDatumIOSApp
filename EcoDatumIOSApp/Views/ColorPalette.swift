63a567c3af710b8fca1f966f61521e471745af21
Ken Wingerden <ken.wingerden@gmail.com>
2019-03-12 21:22:06 -0700

    added icons and color

---
+++ EcoDatumIOSApp/Views/ColorPalette.swift
@@ -0,0 +1,16 @@
+//
+//  ColorPalette.swift
+//  EcoDatumIOSApp
+//
+//  Created by Kenneth Wingerden on 3/12/19.
+//  Copyright © 2019 Kenneth Wingerden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let EDRichBlack = UIColor(named: "EDRichBlack")!
+let EDCerulean = UIColor(named: "EDCerulean")!
+let EDPlatinum = UIColor(named: "EDPlatinum")!
+let EDAsparagus = UIColor(named: "EDAsparagus")!
+let EDCalPolyGreen = UIColor(named: "EDCalPolyGreen")!
